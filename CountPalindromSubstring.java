9c480542bb3db410b083454916db86d256ebebda
LearnerSomya <somyasinghal71001@gmail.com>
2024-01-03 23:55:55 +0530

    Count Palindromic Substring using DP

---
+++ CountPalindromSubstring.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+class CountPalindromSubstring {
+    public static void main(String[] args) throws Exception{
+        Scanner scn = new Scanner(System.in);
+        //taking user enters string
+        System.out.println("Enter string you want to check");
+        String palindromeString = scn.nextLine();
+
+        //using Dynamic Programing concepts
+        //made 2D array so tha could check palindroms in dialgoly backword index
+        boolean[][] array = new boolean[palindromeString.length()][palindromeString.length()]; 
+        int count = 0;
+        //gap will always be less than length of string as we count in normal first character is at position 1 in real life and it its last characher ends at we prograss
+        //like 5- 1 = 4 it will include 5 characters  
+        for(int gap = 0; gap < palindromeString.length(); gap++){
+            // int start = 0;
+            // int end = start + gap;
+
+            for(int start = 0, end = gap; end < array.length; start++,end++){
+                if(gap == 0){
+                    //all characters at gap 0 means are single characters and single characters are always palindrome
+                    array[start][end] = true;
+                } else if(gap == 1){
+                    if(palindromeString.charAt(start) == palindromeString.charAt(end)){
+                        array[start][end] = true;
+                    } else {
+                        array[start][end] = false;
+                    }
+                } else{
+                    //all characters that has gap more than 1 are hadled in this
+                    if(palindromeString.charAt(start) == palindromeString.charAt(end) && array[start + 1][end - 1] == true){
+                        array[start][end] = true;
+                    } else {
+                        array[start][end] = false;
+                    }
+                }
+                if(array[start][end]){
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
