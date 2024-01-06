e85e54d3649c80023221444ae1fd89b17f0f9419
LearnerSomya <somyasinghal71001@gmail.com>
2024-01-06 23:30:47 +0530

    Is number a palindrom

---
+++ PalindromNumber.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+class PalindromNumber {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int num = scn.nextInt();
+        IsNumberPalindrom(num);
+        System.out.println(IsNumberPalindrom(num));
+
+    }
+    public static boolean IsNumberPalindrom(int num){
+        if(num < 0){
+            return false;
+        } else if(num == 0){
+            return true;
+        }
+        int original = num;
+        int remainder = 0;
+        long answer = 0;
+        while(num != 0){
+            
+            remainder = num % 10; // it will give questiont
+            num = num / 10;
+            answer = answer * 10 + remainder; 
+        }
+
+        return answer == original;
+    }
+
+}
