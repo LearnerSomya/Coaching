3019ff78b4b0b3d56e47974dbb6c041202c88f26
LearnerSomya <somyasinghal71001@gmail.com>
2024-01-17 23:29:39 +0530

    Recursion and Faith withBreaking in 4 section

--- LetterCombinatioOfPhoneNumber.java
+++ LetterCombinatioOfPhoneNumber.java
@@ -17,19 +17,20 @@ class LetterCombinatioOfPhoneNumber {
 
     static String[] alphabateCode = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
     //Using Reursion and Faith 
-    public static List<String> getLetterCombination(String digits, int index){
+    static List<String> getLetterCombination(String digits, int index){
         if(index == digits.length()){
             List<String> baseResult = new ArrayList<>();
-            baseResult.add("");
+            baseResult.add(""); //this will add blank as empty string aloso has 1 output i.e. blank, this "" will work for very right number of string user entred => "" + a = a, ""+b = b, ""+c = c => a+g = ag and so on
             return baseResult;
         }
         //so her we start valuation from index + 1 location
         List<String> myResult = new ArrayList<>();//in this list we will hold our result
         List<String> recursionResult = getLetterCombination(digits, index + 1); //if we got 234 the first we have fath that we calculate answer of 
         char characterAtStartingIndex = digits.charAt(index);//here we are calculating value which is befor 34 i.e. 2 here
-        String code = alphabateCode[characterAtStartingIndex - '0']; //this will conatin alphabates at the numbers
+        String code = alphabateCode[characterAtStartingIndex - '0']; //this will conatin alphabates at the numbers //here when we pass a integr that integewr ois not pass but its ascii value is passed
+        //and subtrating 0 from them will give actual integer we pass as string 
 
-        for(String recString: recursionResult){ //reString is predefined
+        for(String recString: recursionResult){ 
             for(int i = 0; i < code.length(); i++){ // here we are getting value in code i.e. alphabates
                 char charaterCode = code.charAt(i);
                 myResult.add(charaterCode + recString);
