4c480a8923a958da1cbebd4b57f56aadb429102c
LearnerSomya <somyasinghal71001@gmail.com>
2024-01-18 21:37:59 +0530

    Pattern Problems

---
+++ PatternBosscoderTwo.java
@@ -0,0 +1,13 @@
+
+import java.io.*;
+import java.util.*;
+class PatternBosscoderTwo  {
+    public static void main(String[] args) {
+        for(int i = 0; i <= 5; i++){
+            for(int j = 0; j <= 5;j++){
+                System.out.print(i+""+j + " ");
+        }
+        System.out.println("");
+        }
+    }
+}
